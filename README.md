4469d62f32a76d9d9638b72f8501b54fcfcd10b6
KJ Jackson <kjjackson619@yahoo.com>
2022-01-20 22:28:17 -0500

    table info

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# employee-database
+# Employee Database
+
+## Purpose
+For a business owner to be able to view and manage the departments, roles, and employees in their company, to assist with organizing and planning their business.
+
+## Built With
+
+
+## Demonstration
+
+
+
+## Contribution 
+Made by Kevin Jackson
---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO department (name)
+VALUES
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Sales Lead', 100000, 1),
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2),
+('Software Engineer', 120000, 2),
+('Account Manager', 160000, 3),
+('Accountant', 125000, 3),
+('Legal Team Lead', 250000, 4),
+('Lawyer', 190000, 4);
+
+
+
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+VALUES
+('John', 'Doe', 1, 0),
+('Mike', 'Chan', 2, 1),
+('Ashley', 'Rodriguez', 3, 0),
+('Kevin', 'Tupik', 4, 3),
+('Kunal', 'Singh', 5, 0),
+('Malia', 'Brown', 6, 5),
+('Sarah', 'Lourd', 7, 0),
+('Tom', 'Allen', 8, 7);
